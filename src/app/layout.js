733c73292461ddb567d00dde6f2a9340b8a91608
ikaay1 <imgbemele@gmail.com>
2024-05-21 19:09:33 -0400

    home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,34 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Nav from "./components/Nav";
-import AppProvider from "./context/appContext";
+import {Inter} from 'next/font/google';
+import './globals.css';
+import Nav from './components/Nav';
+import AppProvider from './context/appContext';
+import {Providers} from './providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ['latin']});
 
 export const metadata = {
-  title: "Mile One - Shop with Videos",
-  description: "Shop with videos, Your first mile towards business success and customer satisfaction.",
+  title: 'Mile One - Shop with Videos',
+  description:
+    'Shop with videos, Your first mile towards business success and customer satisfaction.',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
-      <link rel="icon" href="2.png"/>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link
+          href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
+          rel='stylesheet'
+        />
+        <link rel='icon' href='2.png' />
       </head>
       <AppProvider>
-    
-      <body className={inter.className}>
-        
-  
+        <body className={inter.className}>
+          <Nav />
 
-      <Nav/>
-        
-      <Providers>{children}</Providers>
-      </body>
+          <Providers>{children}</Providers>
+        </body>
       </AppProvider>
     </html>
   );
