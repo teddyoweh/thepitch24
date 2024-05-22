a350d70790da5ee1ff469414e2bd8e9fef258f72
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2024-05-22 11:26:19 -0400

    idpate

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -1,17 +1,25 @@
-'use client';
-import '../styles/landing.scss';
-import {Box, Button, Flex, Icon, Input, Text, Textarea} from '@chakra-ui/react';
-import React, {useRef, useState} from 'react';
-import UploadIcon from '../assets/icons/uploadIcon';
-import TextResult from '../components/TextResult';
+"use client";
+import "../styles/landing.scss";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Input,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
+// import UploadIcon from "../assets/icons/uploadIcon";
+import TextResult from "../components/TextResult";
 
 const text =
-  'The man is wearing a black short-sleeve t-shirt, a silver necklace, and black gloves while he decorates the cake.';
+  "The man is wearing a black short-sleeve t-shirt, a silver necklace, and black gloves while he decorates the cake.";
 
 const Upload = () => {
   const [show, setShow] = useState(false);
-  const [url, setUrl] = useState('');
-  const [state, setState] = useState('');
+  const [url, setUrl] = useState("");
+  const [state, setState] = useState("");
   const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const handleFileChange = (event) => {
@@ -25,15 +33,16 @@ const Upload = () => {
   return (
     <Box>
       <Flex
-        mt='100px'
-        align='center'
-        justify='center'
-        direction='column'
-        mb='100px'
+        mt="100px"
+        align="center"
+        justify="center"
+        direction="column"
+        mb="100px"
       >
         {url ? (
           <video
-            controlsList='nodownload'
+            className="video-prompt"
+            controlsList="nodownload"
             // control
             //make it play on click and if playing then pause
             onClick={(e) => {
@@ -46,37 +55,42 @@ const Upload = () => {
           />
         ) : null}
         <Button
-          mt='1rem'
-          bg='#b77738'
-          color={'white'}
+          mt="1rem"
+          bg="#b77738"
+          color={"white"}
           onClick={handleChoose}
-          px='7'
+          px="7"
         >
-          {!url ? 'Select file' : 'Change file'}
+          {!url ? "Select file" : "Change file"}
         </Button>
         <input
           ref={inputRef}
-          type='file'
+          type="file"
           onChange={handleFileChange}
-          accept='.mp4'
+          accept=".mp4"
           style={{
-            display: 'none',
+            display: "none",
           }}
         />
-        <Textarea
+        <input
+          type="text"
+          className="prompt-input"
+          placeholder="Write a prompt"
+        />
+        {/* <Textarea
           outline='none'
           border='1px solid #141414'
           mt='1rem'
           width='500px'
           height='120px'
           fontFamily={'GT America'}
-        />
+        /> */}
         <Button
-          color='white'
-          width='150px'
-          bg='#000000'
-          fontFamily={'GT America'}
-          mt='1rem'
+          color="white"
+          width="150px"
+          bg="#000000"
+          fontFamily={"GT America"}
+          mt="1rem"
           isLoading={loading}
           onClick={() => {
             setLoading(true);
